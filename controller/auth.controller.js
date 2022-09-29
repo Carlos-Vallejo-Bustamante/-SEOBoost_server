e8cd85de54f530597e4b428d7561c248e5409ef9
Numan Torres <numantorresr@gmail.com>
2022-09-29 13:16:57 +0200

    new route profile created

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,8 +1,6 @@
-const { isValidObjectId } = require('mongoose');
 const UserModel = require('../models/User.model');
 const { signJwt } = require('../utils/jwt.util')
 const bcrypt = require('bcryptjs')
-const SALT = 10
 
 
 const SignupController = (req, res, next) => {
@@ -18,25 +16,26 @@ const SignupController = (req, res, next) => {
         avatar,
         searches
     } = req.body;
-    console.log(req.body);
-    UserModel
-        .create({
-            email,
-            password,
-            role,
-            speciality,
-            yearsOfExperience,
-            name,
-            lastName,
-            linkedin,
-            avatar,
-            searches
+    UserModel.findOne({ email })
+        .then((user) => {
+            if (user) {
+                throw new Error('Email ya en uso');
+            }
+            return UserModel.create({
+                email, password, role, speciality, yearsOfExperience, name, lastName, linkedin, avatar, searches
+            });
         })
         .then(() => {
             res.sendStatus(201);
             res.json('all good here');
         })
-        .catch(next);
+        .catch((err) => {
+            if (err.message === 'Email ya en uso') {
+                res.status(400).json({ errorMessage: err.message });
+                return;
+            }
+            next(err);
+        });
 };
 
 const LoginController = (req, res, next) => {
